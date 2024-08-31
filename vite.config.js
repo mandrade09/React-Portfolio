9777e04b5ace2f2937c83fd5e060265e3b34d004
Matthew  Andrade <mattandrade09@gmail.com>
2024-08-31 15:26:28 -0500

    updated files 4

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
-    rollupOptions: {
-      input: '/public/index.html' // Ensure Vite is looking for index.html
-    }
-  },
   server: {
-    port: 3000, // You can change this to your preferred port
+    port: 3000,
     open: true
   }
-});
+})
